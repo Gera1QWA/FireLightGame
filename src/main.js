333a21315450f498f3284b45a1615a49e0c2f14b
Tomas Garcia <toms_gr@hotmail.com>
2022-11-26 00:09:26 -0600

    add: buttons

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Room1 from "./scenes/Room1.js"
 import Room2 from "./scenes/Room2.js"
 import Room3 from "./scenes/Room3.js"
 import Room4 from "./scenes/Room4.js";
+import Battle from "./scenes/Battle.js";
 
 
 
@@ -28,14 +29,15 @@ const config = {
                                     // se refiere a dónde se pondrá el canvas o lienzo
     pixelArt: true,		            //Diseño con pixeles definidos (no borrosos)
     backgroundColor: "#000000", 	//Color de fondo del canvas ()
-    scene: [
-        Start,
-        Level, 
-        Map,
-        Room1,
-        Room2,
-        Room3,
-        Room4],    //Aquí irá la lista de scenas del juego
+    // scene: [
+    //     Start,
+    //     Level, 
+    //     Map,
+    //     Room1,
+    //     Room2,
+    //     Room3,
+    //     Room4], 
+    scene: [Battle],    //Aquí irá la lista de scenas del juego
     banner:{
         hidePhaser: true,
         text: "#fff00f",
